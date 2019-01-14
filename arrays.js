e6776cecf80043b64e6b1e69dd5cbe70b6e04e03
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-14 16:34:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,12 +6,11 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, elementToAdd) {
 //should return a new array and not modify the original
-  
+
 
 }
 
 function destructivelyAddElementToBeginningOfArray(array, elementToAdd) {
 //should alter the original array that's passed in
 
-  return array;
 }
