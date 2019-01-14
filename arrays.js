c310a494ee03b4a9c7cbd4843802f99db92bd2e4
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-14 16:31:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
  // Both functions take two parameters, an array and an element to add to the
- // beginning of the array, and both functions should add the element to the beginning of
- // the array and then return the whole array.
- 
+ // beginning of the array, and both functions should add the element to the
+ // beginning of the array and then return the whole array.
+
 function addElementToBeginningOfArray(array, elementToAdd) {
 //should return a new array and not modify the original
 
