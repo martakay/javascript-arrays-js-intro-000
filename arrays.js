5d310b8581a571e4332f1d05f03a44fa0681f106
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-14 17:15:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -46,7 +46,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 
 function removeElementFromEndOfArray(array) {
   array.slice(0, array.length - 1);
-  return array;clear
+  return array;
   // return array.slice(0, array.length - 1)
 
 }
